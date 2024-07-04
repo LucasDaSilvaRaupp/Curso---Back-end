7dad0ef7127ec111f9bda8678f8d4e1f946ae12a
LucasDaSilvaRaupp <129192608+LucasDaSilvaRaupp@users.noreply.github.com>
2024-07-04 17:20:13 -0300

    commit "i"

--- index.js
+++ index.js
@@ -15,8 +15,29 @@ resultado = filtrarPares(arr)
 console.log(resultado)*/
 
 const express = require('express')
+const { Client } = require('pg')
+require("dotenv").config()
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
 
 const app = express()
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log(' A conexão funcionou ');
+    }).catch((err) => {
+        console.error(' Erro ao conectar na DB ')
+    });
+};
+connectDB()
 
 app.get('/', function (req, res){
     res.send(' NOSSA API TÁ FUNCIONANDO ')
